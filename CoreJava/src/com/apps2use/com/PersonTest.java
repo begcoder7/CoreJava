69ab8d5427c39886d724425de2028168728a74e5
sekhar <pzn0004@auburn.edu>
2015-06-17 19:30:41 -0500

    first change

---
+++ CoreJava/src/com/apps2use/com/PersonTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.apps2use.com;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author sekhar
+ *
+ */
+public class PersonTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
